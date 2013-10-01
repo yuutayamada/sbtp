81cde775f899ee9b29fca806ef676ca7e73bdba5
Yuta Yamada <cokesboy@gmail.com>
2013-10-01 17:48:32 -0400

    Add comments

--- sbtp.el
+++ sbtp.el
@@ -1,3 +1,25 @@
+;;; sbtp.el ---
+
+;; Copyright (C) 2013 by Yuta Yamada
+
+;; Author: Yuta Yamada <cokesboy"at"gmail.com>
+;; URL: https://github.com/yuutayamada/sbtp
+;; Version: 0.0.1
+;; Keywords: shell, sbt, scala
+
+;;; License:
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+;;
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+;;
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 (require 'term)
 
@@ -28,3 +50,10 @@
   (sbtp-term-send-backspace))
 
 (provide 'sbtp)
+
+;; Local Variables:
+;; coding: utf-8
+;; mode: emacs-lisp
+;; End:
+
+;;; sbtp.el ends here
